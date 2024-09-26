26524cba232bcb6c8b333eed66b26e071489292c
Paolo Mazzon <pdmazzon@gmail.com>
2024-09-26 16:50:33 -0700

    Added camera looking

--- Camera3d.cs
+++ Camera3d.cs
@@ -1,15 +1,37 @@
 using Godot;
 using System;
+using System.Security.AccessControl;
 
 public partial class Camera3d : Camera3D
 {
+	float _direction = 0;
+	float _zdirection = 0;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Input.MouseMode = Input.MouseModeEnum.Hidden;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var currentMousePosition = GetViewport().GetMousePosition();
+		var relativeMousePosition = (GetViewport().GetVisibleRect().Size / 2) - currentMousePosition;
+		Input.WarpMouse(GetViewport().GetVisibleRect().Size / 2);
+
+		_direction += relativeMousePosition.X * 0.01f;
+		_zdirection += relativeMousePosition.Y * 0.01f;
+		_zdirection = Math.Clamp(_zdirection, -((float)Math.PI * 0.4f), (float)Math.PI * 0.4f);
+		
+		LookAt(new Vector3(
+			Position.X - (float)Math.Cos(_direction), 
+			Position.Y + (float)Math.Tan(_zdirection),
+			Position.Z + (float)Math.Sin(_direction)
+		));
+
+		float forward = Input.IsKeyPressed(Key.W) ? 1 : 0;
+		float backward = Input.IsKeyPressed(Key.S) ? -1 : 0;
+		float direction = forward + backward;
 	}
 }
--- TerrainGenerator.cs
+++ TerrainGenerator.cs
@@ -4,15 +4,15 @@ public partial class TerrainGenerator : Node3D
 {
 	MeshInstance3D ground;
 
-	public void AddQuad(SurfaceTool st, float x, float z, float w, float h) 
+	public void AddQuad(SurfaceTool st, Vector2 pos, Vector2 size, Vector4 elevations) 
 	{
-		st.AddVertex(new Vector3(x,     0, z));     // Vertex 0
-        st.AddVertex(new Vector3(x + w, 0, z));     // Vertex 1
-        st.AddVertex(new Vector3(x + w, 0, z + h)); // Vertex 2
+		st.AddVertex(new Vector3(pos.X,          elevations.X, pos.Y));          // Vertex 0
+        st.AddVertex(new Vector3(pos.X + size.X, elevations.Y, pos.Y));          // Vertex 1
+        st.AddVertex(new Vector3(pos.X + size.X, elevations.Z, pos.Y + size.Y)); // Vertex 2
 
-		st.AddVertex(new Vector3(x,     0, z));     // Vertex 0
-        st.AddVertex(new Vector3(x + w, 0, z + h)); // Vertex 2
-        st.AddVertex(new Vector3(x,     0, z + h)); // Vertex 3
+		st.AddVertex(new Vector3(pos.X,          elevations.W, pos.Y));          // Vertex 0
+        st.AddVertex(new Vector3(pos.X + size.X, elevations.Z, pos.Y + size.Y)); // Vertex 2
+        st.AddVertex(new Vector3(pos.X,          elevations.W, pos.Y + size.Y)); // Vertex 3
 	}
 
 	// Called when the node enters the scene tree for the first time.
@@ -22,8 +22,15 @@ public partial class TerrainGenerator : Node3D
         SurfaceTool st = new SurfaceTool();
         st.Begin(Mesh.PrimitiveType.Triangles);
 
-        AddQuad(st, 0, 0, 1, 1);
-		AddQuad(st, 1, 1, 1, 1);
+        AddQuad(st, new Vector2(0, 0), new Vector2(1, 1), new Vector4(0, 0, 0, 1));
+		AddQuad(st, new Vector2(1, 0), new Vector2(1, 1), new Vector4(0, 0, 0, 0));
+		AddQuad(st, new Vector2(2, 0), new Vector2(1, 1), new Vector4(0, 0, 0, 0));
+		AddQuad(st, new Vector2(0, 1), new Vector2(1, 1), new Vector4(0, 0, 0, 0));
+		AddQuad(st, new Vector2(1, 1), new Vector2(1, 1), new Vector4(0, 0, 0, 0));
+		AddQuad(st, new Vector2(2, 1), new Vector2(1, 1), new Vector4(0, 0, 0, 0));
+		AddQuad(st, new Vector2(0, 2), new Vector2(1, 1), new Vector4(0, 0, 0, 0));
+		AddQuad(st, new Vector2(1, 2), new Vector2(1, 1), new Vector4(0, 0, 0, 0));
+		AddQuad(st, new Vector2(2, 2), new Vector2(1, 1), new Vector4(0, 0, 0, 0));
 
         st.GenerateNormals();
         Mesh mesh = st.Commit();
