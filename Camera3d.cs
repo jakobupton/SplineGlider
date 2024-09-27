733e960d9c64141cb6b4f5954e2eb4701fbf8bd8
Jakob Upton <jakejupton@gmail.com>
2024-09-26 20:18:56 -0700

    added freecam movement to Camera3D

--- Camera3d.cs
+++ Camera3d.cs
@@ -15,7 +15,23 @@ public partial class Camera3d : Camera3D
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{
+	{	
+		float forward = Input.IsKeyPressed(Key.W) ? -1 : 0;
+		float backward = Input.IsKeyPressed(Key.S) ? 1 : 0;
+		float left = Input.IsKeyPressed(Key.A) ? -1 : 0;
+		float right = Input.IsKeyPressed(Key.D) ? 1 : 0;
+		float up = Input.IsKeyPressed(Key.Space) ? 1 : 0;
+		float down = Input.IsKeyPressed(Key.Shift) ? -1 : 0;
+
+		float direction = forward + backward;
+		float strafe = left + right;
+		float vertical = up + down;
+
+		var move = new Vector3(strafe, vertical, direction);
+		
+
+		Translate(move.Normalized() * 0.1f);
+
 		var currentMousePosition = GetViewport().GetMousePosition();
 		var relativeMousePosition = (GetViewport().GetVisibleRect().Size / 2) - currentMousePosition;
 		Input.WarpMouse(GetViewport().GetVisibleRect().Size / 2);
@@ -29,9 +45,6 @@ public partial class Camera3d : Camera3D
 			Position.Y + (float)Math.Tan(_ydirection),
 			Position.Z + (float)Math.Sin(_direction)
 		));
-
-		float forward = Input.IsKeyPressed(Key.W) ? 1 : 0;
-		float backward = Input.IsKeyPressed(Key.S) ? -1 : 0;
-		float direction = forward + backward;
+				
 	}
 }
--- TerrainGenerator.cs
+++ TerrainGenerator.cs
@@ -3,24 +3,18 @@ using Godot;
 public partial class TerrainGenerator : Node3D
 {
 	MeshInstance3D ground;
-	Color _color = Color.Color8(0, 240, 200, 1);
-	const int _width = 256;
-    const float _effect = 16;
+	const int _width = 256; // Number of Quads
+    const float _effect = 10; // Height of Quads
+	Texture2D texture;
 
     public void AddQuad(SurfaceTool st, Vector2 pos, Vector2 size, Vector4 elevations) 
 	{
-		st.SetColor(_color);
 		st.AddVertex(new Vector3(pos.X,          elevations.X, pos.Y));          // Vertex 0
-		st.SetColor(_color);
         st.AddVertex(new Vector3(pos.X + size.X, elevations.Y, pos.Y));          // Vertex 1
-		st.SetColor(_color);
         st.AddVertex(new Vector3(pos.X + size.X, elevations.W, pos.Y + size.Y)); // Vertex 2
 
-		st.SetColor(_color);
 		st.AddVertex(new Vector3(pos.X,          elevations.X, pos.Y));          // Vertex 0
-		st.SetColor(_color);
         st.AddVertex(new Vector3(pos.X + size.X, elevations.W, pos.Y + size.Y)); // Vertex 2
-		st.SetColor(_color);
         st.AddVertex(new Vector3(pos.X,          elevations.Z, pos.Y + size.Y)); // Vertex 3
 	}
 
@@ -30,14 +24,34 @@ public partial class TerrainGenerator : Node3D
 		var noise = new FastNoiseLite();
         noise.NoiseType = FastNoiseLite.NoiseTypeEnum.Perlin;
         noise.Frequency = 0.032f;
+		noise.SetFractalOctaves(4);
+
+
+		var img_noise = new Image();
+		for (int z = 0; z < _width; z++)
+		{
+            for (int x = 0; x < _width; x++)
+            {
+                float value = noise.GetNoise2D(x, z);
+                img_noise.SetPixel(x, z, new Color(value, value, value));
+            }
+
+        }
 
         MeshInstance3D land = new MeshInstance3D();
         SurfaceTool st = new SurfaceTool();
         st.Begin(Mesh.PrimitiveType.Triangles);
+		var mat = new StandardMaterial3D();
+        mat.AlbedoColor = new Color(1f, 1f, 1f);
+		st.SetMaterial(mat);
 
-		for (int z = (_width/2)*-1; z < (_width / 2); z++) 
+
+
+
+
+        for (int z = _width/-2; z < (_width / 2); z++) 
 		{
-			for (int x = (_width / 2) * -1; x < (_width / 2); x++) 
+			for (int x = _width / -2; x < (_width / 2); x++) 
 			{
 				AddQuad(st, new Vector2(x, z), new Vector2(1, 1), new Vector4(noise.GetNoise2D(x, z) * _effect, noise.GetNoise2D(x + 1, z) * _effect, noise.GetNoise2D(x, z + 1) * _effect, noise.GetNoise2D(x + 1, z + 1) * _effect));
 			}
