ebc519a8323b500566b400d63f3b6ec2c037e081
IsaacMrowka <i.mrowka12@gmail.com>
2024-10-22 17:41:56 -0700

    implemented redline on glider path

---
+++ RedLine.cs
@@ -0,0 +1,29 @@
+﻿using Godot;
+using System.Collections.Generic;
+
+public partial class RedLine : Sprite3D
+{
+    ImageTexture _tex;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        var img = Image.CreateEmpty(200, 200, false, Image.Format.Rgba8);
+
+        List<Vector3> points = BezierCurve.GetBakedCurve();
+
+
+        // Put the grid contents to the image
+        foreach (var point in points)
+        {
+            img.SetPixel((int)point.X, (int)point.Y, Color.Color8(255, 0, 0));
+        }
+
+        // The output texture
+        _tex = ImageTexture.CreateFromImage(img);
+
+        Texture = _tex;
+        Scale = new Vector3(-100, 100, 1); // Adjust the scale as needed
+
+    }
+}
