f564f3d89eee1e15dc39e1e7d6044061610e2834
Wilson Agyapong <104797896+WilsoAgya@users.noreply.github.com>
2024-10-23 16:23:50 -0700

    added comments

--- Weather.cs
+++ Weather.cs
@@ -3,17 +3,21 @@ using System;
 
 public partial class Weather : Sprite3D
 {
+// Create a reference to the Glider object
     private Glider glider;
 
     public override void _Ready()
     {
+    // Set the glider variable to the glider node through the parent node
         glider = GetParent().GetNode<Glider>("Glider");
     }
 
     public override void _Process(double delta)
     {
+    //Check if glider is a valid reference point / not null
        if(glider != null)
         {
+        //Set position of sprite to position of the glider node
             GlobalPosition = glider.Position;
            
         }
